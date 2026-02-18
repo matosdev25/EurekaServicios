d4c81640560bd8af90e0c25111118e84b58f600e
Martin Matos <matosdev@MacBookPro.lan>
2026-02-18 17:59:34 -0500

    Fix router basename for GitHub Pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "@/index.css";
 import App from "@/App";
+
+const basename = process.env.PUBLIC_URL || "/";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
